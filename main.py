9a5e8e4778b8c76db4ac5783314424891a956226
Alejo Aceval <alejoaceval@frba.utn.edu.ar>
2022-10-12 09:43:19 -0300

    Adaptacion a datos de instrumentos

--- main.py
+++ main.py
@@ -10,51 +10,14 @@ from tensorflow.keras import layers
 from tensorflow.keras import models
 from IPython import display
 
-# Set the seed value for experiment reproducibility.
-seed = 42
-tf.random.set_seed(seed)
-np.random.seed(seed)
 
-# Get speech commands data
-DATASET_PATH = 'data/mini_speech_commands'
-
-data_dir = pathlib.Path(DATASET_PATH)
-if not data_dir.exists():
-  tf.keras.utils.get_file(
-      'mini_speech_commands.zip',
-      origin="http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip",
-      extract=True,
-      cache_dir='.', cache_subdir='data')
-
-commands = np.array(tf.io.gfile.listdir(str(data_dir)))
-commands = commands[commands != 'README.md']
-print('Commands:', commands)
-
-train_ds, val_ds = tf.keras.utils.audio_dataset_from_directory(
-    directory=data_dir,
-    batch_size=64,
-    validation_split=0.2,
-    seed=0,
-    output_sequence_length=16000,
-    subset='both')
-
-label_names = np.array(train_ds.class_names)
-print()
-print("label names:", label_names)
+##
+## Functions
+##
 
 def squeeze(audio, labels):
   audio = tf.squeeze(audio, axis=-1)
   return audio, labels
-
-train_ds = train_ds.map(squeeze, tf.data.AUTOTUNE)
-val_ds = val_ds.map(squeeze, tf.data.AUTOTUNE)
-
-test_ds = val_ds.shard(num_shards=2, index=0)
-val_ds = val_ds.shard(num_shards=2, index=1)
-
-for example_audio, example_labels in train_ds.take(1):  
-  print(example_audio.shape)
-  print(example_labels.shape)
 
 def get_spectrogram(waveform):
   # Convert the waveform to a spectrogram via a STFT.
@@ -68,21 +31,12 @@ def get_spectrogram(waveform):
   spectrogram = spectrogram[..., tf.newaxis]
   return spectrogram
 
-for i in range(3):
-  label = label_names[example_labels[i]]
-  waveform = example_audio[i]
-  spectrogram = get_spectrogram(waveform)
-
-  print('Label:', label)
-  print('Waveform shape:', waveform.shape)
-  print('Spectrogram shape:', spectrogram.shape)
-  print('Audio playback')
-  display.display(display.Audio(waveform, rate=16000))
-
 def plot_spectrogram(spectrogram, ax):
+  print("---Shape: "+str(spectrogram.shape))
   if len(spectrogram.shape) > 2:
-    assert len(spectrogram.shape) == 3
+  #  assert len(spectrogram.shape) == 3
     spectrogram = np.squeeze(spectrogram, axis=-1)
+  print("---Shape: "+str(spectrogram.shape))
   # Convert the frequencies to log scale and transpose, so that the time is
   # represented on the x-axis (columns).
   # Add an epsilon to avoid taking a log of zero.
@@ -93,19 +47,66 @@ def plot_spectrogram(spectrogram, ax):
   Y = range(height)
   ax.pcolormesh(X, Y, log_spec)
 
-
 def make_spec_ds(ds):
   return ds.map(
       map_func=lambda audio,label: (get_spectrogram(audio), label),
       num_parallel_calls=tf.data.AUTOTUNE)
 
+##
+## Start
+##
+
+# Set the seed value for experiment reproducibility.
+seed = 42
+tf.random.set_seed(seed)
+np.random.seed(seed)
+
+# Get instrument data
+DATASET_PATH = 'data/instruments/IRMAS-TrainingData'
+#DATASET_PATH = 'data/mini_speech_commands'
+
+data_dir = pathlib.Path(DATASET_PATH)
+instruments = np.array(tf.io.gfile.listdir(str(data_dir)))
+instruments = instruments[instruments != 'README.txt']
+print('Instruments:', instruments)
+
+# Import instrument data
+train_ds, val_ds = tf.keras.utils.audio_dataset_from_directory(
+    directory=data_dir,
+    batch_size=64,
+    validation_split=0.2,
+    seed=0,
+    output_sequence_length=32000,
+    subset='both')
+
+label_names = np.array(train_ds.class_names)
+print("label names:", label_names)
+
+
+# Squeeze data to drop the extra axis (single channel audio)
+train_ds = train_ds.map(squeeze, tf.data.AUTOTUNE)
+val_ds = val_ds.map(squeeze, tf.data.AUTOTUNE)
+
+print(train_ds.element_spec[0].experimental_as_proto())
+# print
+for e in train_ds:
+  print(e)
+  break
+
+# Split validation set in 2 halves
+test_ds = val_ds.shard(num_shards=2, index=0)
+val_ds = val_ds.shard(num_shards=2, index=1)
+
+# Create spectogram datasets from audio datasets
 train_spectrogram_ds = make_spec_ds(train_ds)
 val_spectrogram_ds = make_spec_ds(val_ds)
 test_spectrogram_ds = make_spec_ds(test_ds)
 
+# Examine the spectrograms for different examples of the dataset
 for example_spectrograms, example_spect_labels in train_spectrogram_ds.take(1):
   break
 
+# Print spectogram datasets
 rows = 3
 cols = 3
 n = rows*cols
@@ -116,6 +117,6 @@ for i in range(n):
     c = i % cols
     ax = axes[r][c]
     plot_spectrogram(example_spectrograms[i].numpy(), ax)
-    ax.set_title(commands[example_spect_labels[i].numpy()])
+    ax.set_title(instruments[example_spect_labels[i].numpy()])
 
 plt.show()
